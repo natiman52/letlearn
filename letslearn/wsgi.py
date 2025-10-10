7e52e07d0117f520ffd4206fab88f5c794451ae4
natiman52 <natnaelyazchew135210@gmail.com>
2025-10-10 19:10:58 +0200

    a must commit

--- letslearn/wsgi.py
+++ letslearn/wsgi.py
@@ -1,15 +1,9 @@
-"""
-WSGI config for letslearn project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+
+
+sys.path.insert(0, os.path.dirname(__file__))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'letslearn.settings')
 
