707aa0d4c88b3ec0a4a874cebf8a496b0e30147d
natiman52 <natnaelyazchew135210@gmail.com>
2025-11-02 13:51:35 +0300

    turned dubug to true

--- letslearn/urls.py
+++ letslearn/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('tutorials.urls')),
 ]
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if(settings.DEBUG==True):
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
