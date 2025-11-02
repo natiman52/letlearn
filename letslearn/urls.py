0dd3545c249451bc4344bc91b06e58ca2dc51624
natiman52 <natnaelyazchew135210@gmail.com>
2025-11-02 13:34:17 +0300

    made sure static is avialble

--- letslearn/urls.py
+++ letslearn/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('tutorials.urls')),
 ]
-if(settings.DEBUG==True):
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
